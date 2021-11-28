deb3fbf166e9f66cd5080314595c81033a1ffb92
maora96 <marinaoliveira.r07@gmail.com>
2021-11-27 23:36:41 -0300

    feat: patient controller + password encrypting

--- src/controllers/auth.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken')
-const Password = require('../utils/password')
-const response = require('../utils/response')
-
-const authenticate = async (req, res) => {
-    
-}
--- src/routes/patient.js
+++ src/routes/patient.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+const Patient = require("../controllers/patient")
+
+const Session = require("../middlewares/session")
 
 // patient routes
-router.post('/', )
+router.post('/', Patient.addPatient)
+router.post('/signin', Patient.signin)
+
+router.get('/:id', Patient.getPatientById)
+router.patch('/:id', Session.auth, Patient.updatePatientById)
+router.delete('/:id', Session.auth, Patient.deletePatientById)
 
 module.exports = router;
